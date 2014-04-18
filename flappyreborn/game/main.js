32e4bc92c7bf0099f107da8c1c4a7a643bc26d11
Arnaj Das <arnajdas92@gmail.com>
2014-04-18 20:36:16 +0530

    With all changes

---
+++ flappyreborn/game/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+var BootState = require('./states/boot');
+var MenuState = require('./states/menu');
+var PlayState = require('./states/play');
+var PreloadState = require('./states/preload');
+
+var game = new Phaser.Game(1200, 490, Phaser.AUTO, 'flappy-bird-reborn');
+
+// Game States
+game.state.add('boot', BootState);
+game.state.add('menu', MenuState);
+game.state.add('play', PlayState);
+game.state.add('preload', PreloadState);
+
+
+game.state.start('boot');
+
+  
